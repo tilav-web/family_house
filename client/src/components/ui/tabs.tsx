e2cc80712c5cdb388e1b6ac1c5f82d31d3f5abbc
Tilav <shavqiddintilovov0@gmail.com>
2026-04-21 21:31:21 +0500

    .

--- client/src/components/ui/tabs.tsx
+++ client/src/components/ui/tabs.tsx
@@ -54,6 +54,7 @@ export const TabsTrigger = React.forwardRef<
   return (
     <button
       ref={ref}
+      type="button"
       onClick={() => value && context.onValueChange(value)}
       className={cn(
         "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
